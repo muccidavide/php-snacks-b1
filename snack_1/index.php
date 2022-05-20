56afa9bc7869ad3b9dbc770bbc0bbbfca9c8b4ea
muccidavide <davidemucci0804@gmail.com>
2022-05-20 12:42:51 +0200

    Add data structure

---
+++ snack_1/index.php
@@ -0,0 +1,21 @@
+<!-- 
+
+## Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
+Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
+Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60
+
+ -->
+
+ <?php 
+ 
+ $match = [
+     [team_1 => 'Olimpia', points_1 => "11",  team_2 => 'Cantù', points_2 =>"21"],
+     [team_1 => 'Virtus', points_1 => "31",  team_2 => 'Dinamo', points_2 =>"27"],
+     [team_1 => 'Brescia', points_1 => "23",  team_2 => 'Varese', points_2 =>"81"],
+     [team_1 => 'Brindisi', points_1 => "26",  team_2 => 'Aquila', points_2 =>"31"],
+ ];
+ 
+ 
+ ?>
